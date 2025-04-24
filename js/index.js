43d55b7f90a3e1c2cfd19fb766883d626b00049d
Your Name <you@example.com>
2025-04-24 16:35:23 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+new Swiper('.swiper',{
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 15,
+    navigation: {
+        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+    }
+});
